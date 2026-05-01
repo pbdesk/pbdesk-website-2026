fa2508d55bfbe8b6f0e086e4918c0471a2e0c62f
Pinal Bhatt <pbdesk@proton.me>
2026-05-01 05:46:49 -0400

    feat: add My Pillers component and integrate into Home page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/home/about";
 import CtaBanner from "@/components/home/cta-banner";
 import Hero from "@/components/home/hero";
+import MyPillers from "@/components/home/my-pillers";
 import MyRealm from "@/components/home/my-realm";
 import MyWellnessThreads from "@/components/home/my-wellness-threads";
 import Pillars from "@/components/home/pillars";
@@ -13,6 +14,7 @@ export default function Home() {
       <MyRealm />
       <About />
       <MyWellnessThreads />
+      <MyPillers />
       <CtaBanner />
     </main>
   );
