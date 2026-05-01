f739d6feb5026e78d5d6aee1b61213beff5c74bd
Pinal Bhatt <pbdesk@proton.me>
2026-05-01 05:20:51 -0400

    feat: add My Wellness Threads component with styles and integrate into Home page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/home/about";
 import CtaBanner from "@/components/home/cta-banner";
 import Hero from "@/components/home/hero";
 import MyRealm from "@/components/home/my-realm";
+import MyWellnessThreads from "@/components/home/my-wellness-threads";
 import Pillars from "@/components/home/pillars";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <Pillars />
       <MyRealm />
       <About />
+      <MyWellnessThreads />
       <CtaBanner />
     </main>
   );
